ff1dc4721998afd504ee15055a195dd24b33bf3f
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2026-02-02 15:18:32 +0100

    commit

---
+++ src/api/todos.api.ts
@@ -0,0 +1,21 @@
+import { api } from "./api";
+import type {Todo} from "../type/types";
+
+export async function getTodos():Promise<Todo[]> {
+    const res = await api.get<Todo[]>("/todos")
+    return res.data
+}
+
+export async function createTodo(payload: Pick<Todo, "title" | "completed">):Promise<Todo>{
+    const res = await api.post<Todo>("/todos", payload)
+    return res.data
+}
+
+export async function updateTodo(id: Todo["id"],patch:Partial<Pick<Todo, "title" | "completed">>):Promise<Todo> {
+    const res = await api.patch<Todo>(`/todos/${id}`, patch)
+    return res.data
+}
+
+export async function deleteTodo(id: Todo["id"]):Promise<void> {
+    await api.delete(`/todos/${id}`)
+}
---
+++ src/type/types.ts
@@ -0,0 +1,9 @@
+export interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+export type CreateTodoPayload = Omit<Todo,"id">
+
+export type UpdateTodoPayload = Partial<CreateTodoPayload>
